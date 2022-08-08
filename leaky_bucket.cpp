8f3cb1605cb5a689290789cd1268bd8e0f46789f
Jordan M <montytyper@msn.com>
2022-08-08 11:34:53 -0700

    Initial code for member functions and bare functions separately.

---
+++ leaky_bucket.cpp
@@ -0,0 +1,47 @@
+#include <functional>
+#include <iostream>
+
+template <typename FunctionType> class Rate_Limiter;
+
+template <typename ReturnValue, typename Obj, typename... Args>
+class Rate_Limiter<ReturnValue (Obj::*)(Args...)> {
+private:
+  std::function<ReturnValue(Args...)> func;
+
+public:
+  Rate_Limiter(Obj cl, ReturnValue (Obj::*member)(Args...))
+      : func{[=](Args... as) mutable { return (cl.*member)(as...); }} {}
+
+  ReturnValue operator()(Args... args) const { return func(args...); }
+};
+
+template <typename FunctionType, FunctionType func> struct RateLimiterBare;
+template <typename ReturnType, typename... Args, ReturnType (*func)(Args...)>
+struct RateLimiterBare<ReturnType (*)(Args...), func> {
+  bool enabled{true};
+  ReturnType operator()(Args... args) {
+    if (enabled) {
+      return func(args...);
+    } else {
+      return ReturnType{};
+    }
+  }
+};
+#define MAKE_WRAPPER(func)                                                     \
+  RateLimiterBare<decltype(&func), func> {}
+
+struct foo {
+  long long bar(int a, long b, long long c) { return a + b + c; }
+};
+
+int foobar(int a) { return a; }
+
+int main() {
+  foo f;
+
+  Rate_Limiter<decltype(&foo::bar)> wfb{f, &foo::bar};
+  auto tmp = MAKE_WRAPPER(foobar);
+
+  std::cout << wfb(1, 2l, 3ll) << std::endl;
+  std::cout << tmp(3) << std::endl;
+}
